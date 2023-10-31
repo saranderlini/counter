3f47ecfd67a963d227bd3dddc9edb9af56604f0d
Sara Anderlini <sara.anderlini@hotmail.com>
2023-10-31 06:44:49 +0100

    including scripts for counting and change on start

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,6 +5,24 @@ var btnPlus = document.querySelector('button#btnPlus');
 var btnMinus = document.querySelector('button#btnMinus');
 var btnReset = document.querySelector('button#btnReset');
 
+//Declaring var for collapsing and functions
+var leadCap = document.querySelector('p#lead');
+function collapseFunc() {
+    resultDiv.style.visibility = 'visible';
+    resultDiv.style.height = 'fit-content';
+    btnMinus.style.height = 'fit-content';
+    btnMinus.style.visibility = 'visible';
+    btnReset.style.height = 'fit-content';
+    btnReset.style.visibility = 'visible';
+    btnPlus.style.height = 'fit-content';
+    btnPlus.style.visibility = 'visible';
+    leadCap.style.display = 'none';
+    btnStart.style.display = 'none';
+}
+btnStart.addEventListener('click', collapseFunc);
+
+
+
 var pointZero = 0;
 var output;
 
@@ -22,10 +40,11 @@ function minusOne() {
     resultDiv.innerHTML = output;
     return output;
 }
+//Associating the sum to the + button
+btnPlus.addEventListener('click', addOne);
+
 //Associating the subtraction to the - button
-btnMinus.addEventListener('click', () => {
-    minusOne();
-})
+btnMinus.addEventListener('click', minusOne)
 
 //Resetting the counter
 btnReset.addEventListener('click', () => {
@@ -34,7 +53,16 @@ btnReset.addEventListener('click', () => {
     return pointZero;
 })
 
-//Associating the sum to the + button
-btnPlus.addEventListener('click', () => {
-    addOne();
-})
+//Associating the maths to arrow keys
+document.onkeydown = function(e) {
+    switch(e.keyCode) {
+        case 38:
+        case 39:
+            addOne();
+            break;
+        case 37:
+        case 40:
+            minusOne();
+            break;
+    }
+}
