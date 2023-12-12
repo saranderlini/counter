7c136e1cd7573434087c0e0a9abcb345a13f2712
Sara Anderlini <109802140+saranderlini@users.noreply.github.com>
2023-12-12 06:42:53 +0100

    mathematical operators and events

---
+++ asset/js/scrip_3bis.js
@@ -0,0 +1,44 @@
+//Script for mathematical operations
+
+//get all the btns excepted the last one
+const allButLastBtns = Array.from(counterBtns).slice(0, -1);
+var pointZero = 0;
+var output;
+
+allButLastBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.textContent == 'Reset') {
+            pointZero = 0;
+        } 
+        else if (button.textContent == '+') {
+            pointZero += 1;
+        } 
+        else if (button.textContent == '-') {
+            pointZero -= 1;
+        } 
+        else if (button.textContent == 'Take me to 4623874692') {
+            pointZero = 4623874692;
+        }  
+        resultPar.textContent = pointZero;
+        return pointZero;
+    })
+})
+
+//Associating the maths to arrow keys
+document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 38:
+        case 39:
+            pointZero += 1;
+            break;
+        case 37:
+        case 40:
+            pointZero -= 1;
+            break;
+            case 82:
+                pointZero = 0;
+                break;
+            }
+    resultPar.textContent = pointZero;
+    return pointZero;
+};
